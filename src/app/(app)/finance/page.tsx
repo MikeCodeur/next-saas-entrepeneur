43c43a7b7278fba95392a00c9c091961e1783e13
Mike Codeur <contact@mikecodeur.com>
2025-01-15 17:08:43 +0800

    prepare exo 6

--- src/app/(app)/finance/page.tsx
+++ src/app/(app)/finance/page.tsx
@@ -1,9 +1,65 @@
-export {default} from './page.exercise'
+import {getUserIdDal} from '@/app/dal/user-dal'
+import FinanceYearSelect from '@/components/dashboard/trackers/finance/finance-year-select'
+import FinanceDataTable from '@/components/dashboard/trackers/finance/finance-data-table'
+import {Separator} from '@/components/ui/separator'
+import {
+  getFinancesWithPaginationByYear,
+  getYearsFinancesByUid,
+} from '@/services/finance-service'
+import {DATA_ROWS_PER_PAGE} from '@/utils/constants'
+import {notFound} from 'next/navigation'
+import React from 'react'
 
-//export {default} from './page.final'
+type SearchParams = Promise<{
+  financeYear?: string
+  page?: string
+  pageSize?: string
+}>
 
-//1. 🚀 Shadcn Data Table (Tanstack)
-//export {default} from './page.bonus-1'
+const Page = async (props: {searchParams?: SearchParams}) => {
+  const userId = await getUserIdDal()
+  if (!userId) notFound()
 
-//2. 🚀 Changement d’années : Select
-//export {default} from './page.bonus-2'
+  const params = await props.searchParams
+  const defaultYear = new Date().getFullYear().toString()
+
+  const page = Number(params?.page) || 1
+  const limit = Number(params?.pageSize) || DATA_ROWS_PER_PAGE
+
+  const years = (await getYearsFinancesByUid(userId)) ?? [
+    {year: new Date().getFullYear().toString()},
+  ]
+
+  const requestedYear = params?.financeYear
+  // Si l'année demandée existe dans years, on l'utilise, sinon on prend la première année disponible
+  const financeYear =
+    requestedYear && years.some((y) => y.year === requestedYear)
+      ? requestedYear
+      : years?.[0]?.year || defaultYear
+
+  const hasCurrentYear = years?.find((year) => year.year === financeYear)?.year
+
+  const finances = await getFinancesWithPaginationByYear(
+    financeYear,
+    userId,
+    page,
+    limit
+  )
+
+  return (
+    <div className="flex w-full flex-col justify-center gap-4 pb-4">
+      <h1 className="text-2xl font-semibold">Finance</h1>
+      <Separator className="my-4" />
+
+      <FinanceYearSelect years={years} currentYear={financeYear}>
+        {hasCurrentYear ? (
+          <FinanceDataTable finances={finances} />
+        ) : (
+          <div>No data for this year</div>
+        )}
+      </FinanceYearSelect>
+    </div>
+  )
+}
+
+export default Page
--- src/components/dashboard/trackers/finance/finance-year-select.tsx
+++ src/components/dashboard/trackers/finance/finance-year-select.tsx
@@ -1,4 +1,52 @@
-export {default} from './finance-year-select.exercise'
+'use client'
 
-//2. 🚀 Changement d’années : Select
-//export {default} from './finance-year-select.bonus-2'
+import {Card, CardContent, CardHeader, CardTitle} from '@/components/ui/card'
+import React, {useEffect} from 'react'
+import {usePathname, useRouter, useSearchParams} from 'next/navigation'
+
+import SelectTime from '@/components/selects/select-time'
+import {pageParamName, yearParamName} from '@/utils/constants'
+
+type FinanceChartTriggerProps = {
+  years: {
+    year: string
+  }[]
+  currentYear?: string
+  children?: React.ReactNode
+}
+
+const FinanceYearSelect = ({
+  years,
+  currentYear,
+  children,
+}: FinanceChartTriggerProps) => {
+  const router = useRouter()
+  const searchParams = useSearchParams()
+  const pathname = usePathname()
+
+  const handleChangeYear = (newYear: string) => {
+    if (currentYear === newYear) return
+
+    const params = new URLSearchParams(searchParams)
+    params.set(yearParamName, newYear)
+    params.set(pageParamName, '1') // Retour à la première page
+    router.push(`${pathname}?${params.toString()}`, {scroll: false})
+  }
+
+  return (
+    <Card>
+      <CardHeader className="space-y-4 text-xl font-semibold">
+        <CardTitle>Finance</CardTitle>
+        <SelectTime
+          currentTime={currentYear ?? years?.[0].year}
+          times={years}
+          handleChange={handleChangeYear}
+          placeholder="Sélectionnez une année"
+        />
+      </CardHeader>
+      <CardContent className="p-4">{children}</CardContent>
+    </Card>
+  )
+}
+
+export default FinanceYearSelect
